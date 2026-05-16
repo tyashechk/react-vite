bf79f9ff5299b7b11f896023fa5e585266092cc1
tyashechk <konarevatoma@gmail.com>
2026-05-16 11:14:11 +0300

    kk

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import './Home.scss';
+
+const Home = () => {
+    return <div className="home-page">
+        <div className="container">
+            <h1>Главная страница</h1>
+            <p>Здесь будет контент главной страницы</p>
+        </div>
+    </div>
+};
+
+export default Home;
